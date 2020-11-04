0d68e2e8d0767622a29b13f8d61915ae65e06cd2
sic9202 <70256152+sic9202@users.noreply.github.com>
2020-11-04 23:32:08 +0900

    signUpController, MemberVO, servlet-context, pom.xml, root-context, jdbc.properties, MemberDAO, member.xml 임시 수정

--- Lapflix/src/main/java/com/nerdofthree/Lapflix/signUpController.java
+++ Lapflix/src/main/java/com/nerdofthree/Lapflix/signUpController.java
@@ -1,13 +1,29 @@
 package com.nerdofthree.Lapflix;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import mybatis.dao.MemberDAO;
 
 @Controller
 public class signUpController {
-    @RequestMapping("/signUp")
+    
+	@Autowired
+	private MemberDAO m_dao;
+	
+	@RequestMapping("/signUp")
     public String signUp(){
     	
         return "sign_up";
     }
+	
+	@RequestMapping(value = "/singUp", method = RequestMethod.POST)
+	public String signUp_ok(String name, String age, String gender, String f_key, String email, String p_image) {
+		
+		m_dao.add(name, age, gender, f_key, email, p_image);
+		
+		return "sign_in";
+	}
 }
--- Lapflix/src/main/java/mybatis/dao/MemberDAO.java
+++ Lapflix/src/main/java/mybatis/dao/MemberDAO.java
@@ -12,19 +12,18 @@ public class MemberDAO {
 	private SqlSessionTemplate sst;
 	
 	//회원가입
-	public boolean add(String name, int age, String gender, int f_key, String email, String profile_image ) {
-		
-		boolean chk = false;
+	public void add(String name, String age, String gender, String f_key, String email, String p_image ) {
 		
 		Map<String, String> map = new HashMap<String, String>();
 		
-		map.put("member_name", name);
-		map.put("member_age", name);
-		map.put("member_gender", name);
-		map.put("f_key", String.valueOf(f_key));
+		map.put("name", name);
+		map.put("age", age);
+		map.put("gender", gender);
+		map.put("f_key", f_key);
 		map.put("email", email);
-		map.put("profile_image", profile_image);
+		map.put("p_image", p_image);
 		
-		return chk;
+		int cnt = sst.insert("member.add", map);
+
 	}
 }
